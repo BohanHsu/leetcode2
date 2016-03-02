ddd821400cc8a75b365ad836cee342916b1d3aa9
BohanHsu <ilovebarce@gmail.com>
2016-03-01 19:54:26 -0500

    work long time ago

---
+++ best_time_to_buy_and_sell_stock_with_cooldown.rb
@@ -0,0 +1,4 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+end
--- test/best_time_to_buy_and_sell_stock_with_cooldown_test.rb
+++ test/best_time_to_buy_and_sell_stock_with_cooldown_test.rb
@@ -9,17 +9,17 @@ describe 'max_profit(prices)' do
   end
 
   it 'should pass failed test case' do
-    #prices = [6,1,3,2,4,7]
-    #expected_result = 6
-    #max_profit(prices).must_equal(expected_result)
+    prices = [6,1,3,2,4,7]
+    expected_result = 6
+    max_profit(prices).must_equal(expected_result)
 
-    #prices = [2,1,4,5,2,9,7]
-    #expected_result = 10
-    #max_profit(prices).must_equal(expected_result)
+    prices = [2,1,4,5,2,9,7]
+    expected_result = 10
+    max_profit(prices).must_equal(expected_result)
 
-    #prices = [1,3,5,4,3,7,6,9,2,4]
-    #expected_result = 10
-    #max_profit(prices).must_equal(expected_result)
+    prices = [1,3,5,4,3,7,6,9,2,4]
+    expected_result = 10
+    max_profit(prices).must_equal(expected_result)
 
     prices = [8,6,4,3,3,2,3,5,8,3,8,2,6]
     expected_result = 10
@@ -42,6 +42,12 @@ describe 'max_profit(prices)' do
 
     prices = [1,3,2]
     expected_result = 2
+    max_profit(prices).must_equal(expected_result)
+  end
+
+  it 'should pass new test case' do
+    prices = [1,2,1,1,3]
+    expected_result = 3
     max_profit(prices).must_equal(expected_result)
   end
 end
