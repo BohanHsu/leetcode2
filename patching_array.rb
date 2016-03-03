4ab0c59b8a38e71e483e92386c4d3229331494e8
U-NETAPP\bohan <bohan@VIDHIB-PC.hq.netapp.com>
2016-03-03 16:02:11 -0500

    current work, pass all local test cases

--- patching_array.rb
+++ patching_array.rb
@@ -3,28 +3,48 @@
 # @return {Integer}
 def min_patches(nums, n)
   if nums.length == 0
+    sum = 3
+    count = 2
+    i = 0
+  else
+    if nums[0] == 1
+      sum = 1
+      count = 0
+      i = 1
+      if nums.length >=2 && (nums[1] == 1 || nums[1] == 2)
+        sum += nums[1]
+        i = 2
+      end
+    elsif nums[0] == 2
+      sum = 3
+      count = 1
+      i = 1
+    else
+      sum = 1
+      count = 1
+      i = 0
+    end
   end
 
-  count = 0
-  sum = 0
-  base = 1
-  while base * 2 < nums[0] do
-    puts "add: #{base}"
-    count += 1
-    sum += base
-    base *= 2
-  end
-
-  i = 0
-  while sum < n do
-    if nums[i] < sum * 2 + 1
+  while i < nums.length do
+    if nums[i] <= sum + 1
       sum += nums[i]
       i += 1
     else
+      #puts "add1: #{sum+1}"
       sum = sum * 2 + 1
-      puts "add: #{sum}"
       count += 1
     end
+    if sum >= n
+      return count
+    end
   end
+
+  while sum < n do
+    #puts "add2: #{sum+1}"
+    sum = sum * 2 + 1
+    count += 1
+  end
+
   return count
 end
--- test/patching_array_test.rb
+++ test/patching_array_test.rb
@@ -3,9 +3,53 @@ require '../patching_array'
 
 describe 'min_patches(nums, n)' do
   it 'should work1' do
-    nums = [1,5, 20]
+    nums = [1,5,10]
     n = 20
     expected_result = 2
+    min_patches(nums, n).must_equal(expected_result)
+
+    nums = [1,3]
+    n = 6
+    expected_result = 1
+    min_patches(nums, n).must_equal(expected_result)
+
+    nums = [1,2,2]
+    n = 5
+    expected_result = 0
+    min_patches(nums, n).must_equal(expected_result)
+
+    nums = [1,1,4]
+    n = 10
+    expected_result = 2
+    min_patches(nums, n).must_equal(expected_result)
+  end
+
+  it 'should work2' do
+    nums = []
+    n = 10
+    expected_result = 4
+    min_patches(nums, n).must_equal(expected_result)
+
+    nums = [1]
+    n = 10
+    expected_result = 3
+    min_patches(nums, n).must_equal(expected_result)
+
+    nums = [2]
+    n = 10
+    expected_result = 3
+    min_patches(nums, n).must_equal(expected_result)
+
+    nums = [4]
+    n = 10
+    expected_result = 3
+    min_patches(nums, n).must_equal(expected_result)
+  end
+
+  it 'should work3' do
+    nums = [1,2,32]
+    n = 2147483647
+    expected_result = 28
     min_patches(nums, n).must_equal(expected_result)
   end
 end
