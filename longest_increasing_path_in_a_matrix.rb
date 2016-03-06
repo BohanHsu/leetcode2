ab47687c4bbbabc1c916da090f61cbe562ade390
BohanHsu <ilovebarce@gmail.com>
2016-03-06 12:19:33 -0500

    pass, inspired by someone online, actually dp

--- longest_increasing_path_in_a_matrix.rb
+++ longest_increasing_path_in_a_matrix.rb
@@ -5,87 +5,45 @@ def longest_increasing_path(matrix)
   return 0 if matrix.first.length == 0
 
   height = matrix.length
-  width = matrix.first.length
+  width = matrix[0].length
   max = -1
 
   path_length = matrix.map do |row|
     row.map do
-      0
+      nil
     end
   end
 
-  queue = find_start_position(matrix, path_length)
-    
-  while !queue.empty? do
-    pos = queue.shift
-    i = pos[0]
-    j = pos[1]
-
-    if path_length[i][j] > max
-      max = path_length[i][j]
-    end
-
-
-    if i - 1 >= 0 && matrix[i-1][j] > matrix[i][j]
-      if path_length[i-1][j] < path_length[i][j] + 1
-        path_length[i-1][j] = path_length[i][j] + 1
-        queue << [i-1, j]
-      end
-    end
-    if i + 1 < height && matrix[i+1][j] > matrix[i][j]
-      if path_length[i+1][j] < path_length[i][j] + 1
-        path_length[i+1][j] = path_length[i][j] + 1
-        queue << [i+1, j]
-      end
-    end
-    if j - 1 >= 0 && matrix[i][j-1] > matrix[i][j]
-      if path_length[i][j-1] < path_length[i][j] + 1
-        path_length[i][j-1] = path_length[i][j] + 1
-        queue << [i, j-1]
-      end
-    end
-    if j + 1< width && matrix[i][j+1] > matrix[i][j]
-      if path_length[i][j+1] < path_length[i][j] + 1
-        path_length[i][j+1] = path_length[i][j] + 1
-        queue << [i, j+1]
-      end
+  height.times do |i|
+    width.times do |j|
+      dfs(matrix, path_length, i, j)
+      max = [max, path_length[i][j]].max
     end
   end
+
   max
 end
 
-def find_start_position(matrix, path_length)
-  height = matrix.length
-  width = matrix.first.length
-
-  queue = []
-  height.times do |i|
-    width.times do |j|
-      if start_position?(matrix, i, j)
-        path_length[i][j] = 1
-        queue << [i, j] 
-      end
-    end
+def dfs(matrix, path_length, i, j)
+  if path_length[i][j].nil?
+    max_neighboor = neighboors(matrix, i, j).select do |pos|
+      matrix[pos[0]][pos[1]] > matrix[i][j]
+    end.map do |pos|
+      dfs(matrix, path_length, pos[0], pos[1])
+    end.max
+    path_length[i][j] = max_neighboor.nil? ? 1 : max_neighboor + 1
   end
-
-
-  return queue
+  return path_length[i][j]
 end
 
-def start_position?(matrix, i, j)
+def neighboors(matrix, i, j)
   height = matrix.length
-  width = matrix.first.length
-  if i - 1 >= 0 && matrix[i-1][j] < matrix[i][j]
-    return false
-  end
-  if i + 1 < height && matrix[i+1][j] < matrix[i][j]
-    return false
-  end
-  if j - 1 >= 0 && matrix[i][j-1] < matrix[i][j]
-    return false
-  end
-  if j + 1< width && matrix[i][j+1] < matrix[i][j]
-    return false
-  end
-  return true
+  width = matrix[0].length
+  result = []
+
+  result << [i-1, j] if i - 1 >= 0
+  result << [i+1, j] if i + 1 < height
+  result << [i, j - 1] if j - 1 >= 0
+  result << [i, j + 1] if j + 1 < width
+  result
 end
