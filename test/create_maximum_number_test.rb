e2fc993663ee14f052c39ad7256d0e948e2979bf
BohanHsu <ilovebarce@gmail.com>
2016-03-10 21:37:59 -0500

    pass because see someone's work

--- test/create_maximum_number_test.rb
+++ test/create_maximum_number_test.rb
@@ -57,3 +57,25 @@ describe 'compare(nums1, nums2)' do
     compare(nums1, nums2).must_equal(expected_result)
   end
 end
+
+describe 'max_number(nums1, nums2, k)' do
+  it 'should work1' do
+    nums1 = [3, 4, 6, 5]
+    nums2 = [9, 1, 2, 5, 8, 3]
+    k = 5
+    expected_result = [9, 8, 6, 5, 3]
+    max_number(nums1, nums2, k).must_equal(expected_result)
+
+    nums1 = [6, 7]
+    nums2 = [6, 0, 4]
+    k = 5
+    expected_result = [6, 7, 6, 0, 4]
+    max_number(nums1, nums2, k).must_equal(expected_result)
+
+    nums1 = [3, 9]
+    nums2 = [8, 9]
+    k = 3
+    expected_result = [9, 8, 9]
+    max_number(nums1, nums2, k).must_equal(expected_result)
+  end
+end
