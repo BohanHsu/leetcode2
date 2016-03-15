f3cbf595f0d76c67291632d409ea0309930876dc
BohanHsu <ilovebarce@gmail.com>
2016-03-14 23:10:16 -0400

    half

--- remove_duplicate_letters.rb
+++ remove_duplicate_letters.rb
@@ -1,16 +1,33 @@
 # @param {String} s
 # @return {String}
 def remove_duplicate_letters(s)
-  letters = 0
-  s.chars.each do |char|
-    letters |= 1 << (char.ord - 97)
-  end
-
-  str = ""
-  (0...26).each do |i|
-    if (letters & (1 << i)) != 0
-      str += (i + 97).chr
+  matrix = 26.times.map do
+    26.times.map do
+      false
     end
   end
-  str
+  idx_before = {}
+
+  s.chars.each do |char|
+    cur_idx = char.ord - 97
+    idx_before.keys.each do |k|
+      if k != cur_idx
+        matrix[cur_idx][k] = true
+      end
+    end
+    if idx_before[cur_idx].nil?
+      idx_before[cur_idx] = false
+    else
+      idx_before[cur_idx] = true
+    end
+  end
+
+  stack = []
+  s.chars.reverse.each do |char|
+    stack << (char.ord - 97)
+    if !idx_before[(char.ord - 97)]
+      break
+    end
+  end
+
 end
