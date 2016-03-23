c3c9154e7bbf6395d3ddc4f114c169854c4f0cf1
BohanHsu <ilovebarce@gmail.com>
2016-03-22 21:25:07 -0400

    pass

--- additive_number.rb
+++ additive_number.rb
@@ -1,19 +1,17 @@
 # @param {String} num
 # @return {Boolean}
 def is_additive_number(num)
+  return false if num.length < 3
   stack = [[0,0]]
   i = stack[-1][1]
 
   while stack[-1][1] != num.length - 1 do
-    #puts "stack=#{stack}"
     match_flag = false
     (i+1..(num.length - 1 - i) / 2 + i + 1) .each do |j|
-      #puts "num1=#{num[stack[-1][0]..stack[-1][1]].to_i}, num2=#{num[i+1..j].to_i}"
       cur_sum = (num[stack[-1][0]..stack[-1][1]].to_i + num[i+1..j].to_i).to_s
       cur_sum_length = cur_sum.length
-      #puts "next=#{num[j+1..j+cur_sum_length]}, cur_sum=#{cur_sum}"
 
-      if num[j+1..j+cur_sum_length] == cur_sum
+      if num[j+1..j+cur_sum_length] == cur_sum && (num[stack[-1][0]] != '0' || stack[-1][0] == stack[-1][1]) && (num[i+1] != '0' || i+1 == j)
         stack << [i+1, j]
         match_flag = true
         return true if j + cur_sum_length == num.length - 1
@@ -31,6 +29,7 @@ def is_additive_number(num)
         return false if stack.empty?
         stack[-1][1] += 1
       end
+      i = stack[-1][1]
     end
   end
   true
--- test/additive_number_test.rb
+++ test/additive_number_test.rb
@@ -15,4 +15,26 @@ describe 'is_additive_number(num)' do
     expected_result = false
     is_additive_number(num).must_equal(expected_result)
   end
+
+  it 'should work2' do
+    num = '0'
+    expected_result = false
+    is_additive_number(num).must_equal(expected_result)
+  end
+
+  it 'should work3' do
+    num = '1023'
+    expected_result = false
+    is_additive_number(num).must_equal(expected_result)
+
+    num = '101'
+    expected_result = true
+    is_additive_number(num).must_equal(expected_result)
+  end
+
+  it 'should work4' do
+    num = '211738'
+    expected_result = true
+    is_additive_number(num).must_equal(expected_result)
+  end
 end
