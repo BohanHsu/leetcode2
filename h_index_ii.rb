a7b40b99f297598098b660f09231a240b636b3ba
U-NETAPP\bohan <bohan@VIDHIB-PC.hq.netapp.com>
2016-04-07 16:42:02 -0400

    pass

--- h_index_ii.rb
+++ h_index_ii.rb
@@ -1,11 +1,50 @@
 # @param {Integer[]} citations
 # @return {Integer}
 def h_index(citations)
-  citations.each_with_index do |citation, i|
-    if citation >= citations.length - i
-      return citations.length - i
+  return 0 if citations.empty?
+  from = 0
+  to = citations.length
+  mid = (from + to) / 2
+
+  count = 0
+  last_mid = nil
+  while !check_h_index(mid, citations) do
+    return if count > 10
+
+    direction = get_direction(mid, citations)
+
+    if direction == :move_to
+      to = mid
+    elsif direction == :move_from
+      from = mid
+    else
+      return 0
     end
+    if (from + to) / 2 == last_mid
+      return 0
+    end
+
+    last_mid = mid
+    mid = (from + to) / 2
   end
 
-  0
+  citations.length - mid
 end
+
+def get_direction(idx, citations)
+  if idx == 0
+    return nil
+  else
+    if citations[idx - 1] >= citations.length - idx + 1 && citations[idx] >= citations.length - idx
+      return :move_to
+    elsif citations[idx - 1] < citations.length - idx + 1 && citations[idx] < citations.length - idx
+      return :move_from
+    end
+  end
+  nil
+end
+
+def check_h_index(idx, citations)
+  idx == 0 && citations[0] >= citations.length || citations[idx - 1] < citations.length - idx + 1 && citations[idx] >= citations.length - idx
+end
+
