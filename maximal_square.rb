3c6fef71dea9f0b527a8e6bfffc0dfc6f222ffc0
BohanHsu <ilovebarce@gmail.com>
2016-04-26 22:44:58 -0400

    fuck

--- maximal_square.rb
+++ maximal_square.rb
@@ -1,6 +1,19 @@
 # @param {Character[][]} matrix
 # @return {Integer}
 def maximal_square(matrix)
+  return 0 if matrix.length == 0 || matrix[0].length == 0
+
+
+  if matrix[0].class == String
+    matrix = [matrix]
+  end
+
+  matrix.each do |row|
+    row.map! do |chr|
+      chr.to_i
+    end
+  end
+
   cur_line = matrix[0]
   max = max_in_line cur_line
   (1...matrix.length).each do |i|
@@ -12,7 +25,7 @@ def maximal_square(matrix)
         cur_line[j] + 1
       end
     end
-    max = [max, max_in_line cur_line].max
+    max = [max, max_in_line(cur_line)].max
   end
   max
 end
@@ -23,17 +36,20 @@ def max_in_line(line)
 
   line.each_with_index do |num, i|
     while !stack.empty? && line[stack[-1]] >= num do
-      area = num * (i - stack.pop + 1)
-      max = [max, area].max
+      j = stack.pop
+      max = [max, [(i - j), line[j]].min ** 2].max
     end
+    max = [[num, (i - (stack.empty? ? -1 : stack[-1]))].min ** 2, max].max
     stack << i
   end
 
   queue = stack
   while !queue.empty? do
-    area = (queue[-1] - queue[0] + 1) * line[queue.shift]
-    max = [max, area].max
+    j = queue[0]
+    while j >= 1 && line[j-1] == line[j] do
+      j -= 1
+    end
+    max = [max, [(line.length - 1 - j  + 1), line[queue.shift]].min ** 2].max
   end
-
   max
 end
--- test/maximal_square_test.rb
+++ test/maximal_square_test.rb
@@ -1,5 +1,82 @@
 require 'minitest/autorun'
 require '../maximal_square'
 
-describe '' do
+describe 'max_in_line(line)' do
+  it 'should work1' do
+    line = [1,2,3,4,99,100,101,1,2]
+    expected_result = 16
+    max_in_line(line).must_equal(expected_result)
+
+    line = [1,2,3,4,101,100,99,1,2]
+    expected_result = 16
+    max_in_line(line).must_equal(expected_result)
+
+    line = [101,100,99,1,2]
+    expected_result = 9
+    max_in_line(line).must_equal(expected_result)
+
+    line = [99,100,99,1,2]
+    expected_result = 9
+    max_in_line(line).must_equal(expected_result)
+
+    line = [99,100,101,1,2]
+    expected_result = 9
+    max_in_line(line).must_equal(expected_result)
+
+    line = [99,100,101]
+    expected_result = 9
+    max_in_line(line).must_equal(expected_result)
+
+    line = [101,100,99]
+    expected_result = 9
+    max_in_line(line).must_equal(expected_result)
+
+    line = [101,100,99,50,50,50,50]
+    expected_result = 49
+    max_in_line(line).must_equal(expected_result)
+
+    line = [99,100,101,50,50,50,50]
+    expected_result = 49
+    max_in_line(line).must_equal(expected_result)
+
+    line = [50,50,50,50,99,100,101]
+    expected_result = 49
+    max_in_line(line).must_equal(expected_result)
+
+    line = [50,50,50,50,101,100,99]
+    expected_result = 49
+    max_in_line(line).must_equal(expected_result)
+  end
+
+  describe 'maximal_square(matrix)' do
+    it 'should work2' do
+      matrix = [[1,0,1,0,0],
+      [1,0,1,1,1],
+      [1,1,1,1,1],
+      [1,0,0,1,0]]
+
+      matrix.each do |row|
+        row.map! do |i|
+          "#{i}"
+        end
+      end
+
+      expected_result = 4
+      maximal_square(matrix).must_equal(expected_result)
+
+      matrix = []
+      expected_result = 0
+      maximal_square(matrix).must_equal(expected_result)
+
+      matrix = [[]]
+      expected_result = 0
+      maximal_square(matrix).must_equal(expected_result)
+    end
+
+    it 'should work3' do
+      matrix = ["0"]
+      expected_result = 0
+      maximal_square(matrix).must_equal(expected_result)
+    end
+  end
 end
