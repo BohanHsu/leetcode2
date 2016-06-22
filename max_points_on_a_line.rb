58dac0489b9f9af0d17b52d391bb40f5765d0783
BohanHsu <ilovebarce@gmail.com>
2016-06-21 23:00:15 -0400

    wip

--- max_points_on_a_line.rb
+++ max_points_on_a_line.rb
@@ -10,7 +10,7 @@ end
 # @param {Point[]} points
 # @return {Integer}
 def max_points(points)
-  cur_max = 0
+  cur_max = points.empty? ? 0 : 1
   lines = {}
   vertical = {}
 
@@ -18,27 +18,33 @@ def max_points(points)
     j.times do |i|
       if points[j].x != points[i].x
         a = (points[j].y - points[i].y).to_f / (points[j].x - points[i].x).to_f
-        b = points[j].y - points[i].x * a
+        b = points[j].y - points[j].x * a
+
+        #puts "i=#{i}, j=#{j}, a=#{a}, b=#{b}"
 
         if !(lines.has_key?(a) && lines[a].has_key?(b))
           lines[a] = {} if !lines.has_key?(a)
-          lines[a][b] = 2
+          lines[a][b] = 1
           
-          ((j + 1)...points.length).each do |k|
-            lines[a][b] = lines[a][b] + 1 if points[k].y = points[k].x * a + b
+          ((i + 1)...points.length).each do |k|
+            if points[k].y == points[k].x * a + b
+              lines[a][b] = lines[a][b] + 1 
+              #puts "k=#{k}, points[k].y=#{points[k].y}, points[k].x=#{points[k].x}, a=#{a}, b=#{b}"
+            end
           end
+          cur_max = [cur_max, lines[a][b]].max
         end
-        cur_max = [cur_max, lines[a][b]].max
       else
         x_val = points[j].x
         if !vertical.has_key?(x_val)
-          vertical[x_val] = 2
-          ((j + 1)...points.length).each do |k|
+          vertical[x_val] = 1
+          ((i + 1)...points.length).each do |k|
             vertical[x_val] = vertical[x_val] + 1 if points[k].x == x_val
           end
+          cur_max = [cur_max, vertical[x_val]].max
         end
-        cur_max = [cur_max, vertical[x_val]]
       end
+      #puts "i=#{i}, j=#{j}, cur_max=#{cur_max}"
     end
   end
   cur_max
--- test/max_points_on_a_line_test.rb
+++ test/max_points_on_a_line_test.rb
@@ -3,6 +3,57 @@ require '../max_points_on_a_line'
 
 describe 'max_points(points)' do
   it 'should work1' do
-    max_points(points)
+    array = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5]]
+    points = convert_array_to_points(array)
+    expected_result = 5
+    max_points(points).must_equal(expected_result)
+
+    array = [[0, 0], [2, 4], [4, 8], [6, 12], [8, 16]]
+    points = convert_array_to_points(array)
+    expected_result = 5
+    max_points(points).must_equal(expected_result)
+
+    array = [[0, 1], [2, 2], [4, 3], [6, 4], [8, 4]]
+    points = convert_array_to_points(array)
+    expected_result = 4
+    max_points(points).must_equal(expected_result)
+
+    array = [[1, 1], [2, 1], [4, 1], [6, 1], [8, 1]]
+    points = convert_array_to_points(array)
+    expected_result = 5
+    max_points(points).must_equal(expected_result)
+
+    array = []
+    points = convert_array_to_points(array)
+    expected_result = 0
+    max_points(points).must_equal(expected_result)
+
+    array = [[1, 1]]
+    points = convert_array_to_points(array)
+    expected_result = 1
+    max_points(points).must_equal(expected_result)
+
+    array = [[1,1],[1,1],[2,3]]
+    points = convert_array_to_points(array)
+    expected_result = 3
+    max_points(points).must_equal(expected_result)
+
+    array = [[1,1],[1,1],[1,3]]
+    points = convert_array_to_points(array)
+    expected_result = 3
+    max_points(points).must_equal(expected_result)
+  end
+
+  it 'should work2' do
+    array = [[3,1],[12,3],[3,1],[-6,-1]]
+    points = convert_array_to_points(array)
+    expected_result = 4
+    max_points(points).must_equal(expected_result)
+  end
+end
+
+def convert_array_to_points(array)
+  return array.map do |pair|
+    Point.new(pair[0], pair[1])
   end
 end
