4bd29786e9671266249d5e28ca2f146f72fbf7ec
U-NETAPP\bohan <bohan@VIDHIB-PC.hq.netapp.com>
2016-07-13 23:30:36 -0400

    wip

---
+++ surrounded_regions.rb
@@ -0,0 +1,66 @@
+# @param {Character[][]} board
+# @return {Void} Do not return anything, modify board in-place instead.
+def solve(board)
+  #'X', 'O'
+  #'S' == surround
+  #'B' == free
+  
+  height = board.length
+  return if height == 0
+  width = board[0].length
+  return if width == 0
+
+  height.times do |i|
+    width.times do |j|
+      if board[i][j] == 'O'
+        surround = true
+        path = []
+        queue = [[i, j]]
+
+        while !queue.empty? do
+          a, b = queue.shift
+          path << [a, b]
+          if at_border?(height, width, a, b)
+            surround = false
+          end
+
+          queue << [a, b+1] if in_border?(height, width, a, b+1) && board[a][b+1] == 'O'
+          queue << [a, b-1] if in_border?(height, width, a, b-1) && board[a][b-1] == 'O'
+          queue << [a+1, b] if in_border?(height, width, a+1, b) && board[a+1][b] == 'O'
+          queue << [a-1, b] if in_border?(height, width, a-1, b) && board[a-1][b] == 'O'
+        end
+
+        if surround
+          chr = 'S'
+        else
+          chr = 'B'
+        end
+
+        path.each do |pair|
+          a, b = pair
+          board[a][b] = chr
+        end
+      end
+    end
+  end
+
+  height.times do |i|
+    width.times do |j|
+      if board[i][j] == 'S'
+        board[i][j] = 'X'
+      elsif board[i][j] == 'B'
+        board[i][j] = 'O'
+      end
+    end
+  end
+
+  nil
+end
+
+def at_border?(height, width, i, j)
+  return i == 0 || j == 0 || i == height - 1 || j == width - 1
+end
+
+def in_border?(height, width, i, j)
+  return i >= 0 && i < height && j >= 0 && j < width
+end
