6be3b4208d8a9445b206e03b902d4834772076d5
BohanHsu <ilovebarce@gmail.com>
2016-07-24 20:52:19 -0400

    wip

---
+++ convert_sorted_array_to_binary_search_tree.rb
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode
+  attr_accessor :val, :left, :right
+  def initialize(val)
+    @val = val
+    @left, @right = nil, nil
+  end
+end
+
+# @param {Integer[]} nums
+# @return {TreeNode}
+def sorted_array_to_bst(nums)
+
+end
+
+def sorted_array_to_bst_helper(nums)
+end
