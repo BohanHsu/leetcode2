61edfcd5fece53daec4bc7598c2a09d66ed82af8
U-NETAPP\bohan <bohan@VIDHIB-PC.hq.netapp.com>
2016-07-26 22:29:38 -0400

    wip

---
+++ construct_binary_tree_from_preorder_and_inorder_traversal.rb
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+class TreeNode
+  attr_accessor :val, :left, :right
+  def initialize(val)
+    @val = val
+    @left, @right = nil, nil
+  end
+end
+
+# @param {Integer[]} preorder
+# @param {Integer[]} inorder
+# @return {TreeNode}
+def build_tree(preorder, inorder)
+  inorder_index = {}
+  inorder.each_with_index do |e, i|
+    inorder_index[e] = i
+  end
+
+  return build_tree_helper(inorder, 0, inorder.length-1, preorder, 0, preorder.length-1, inorder_index)
+end
+
+def build_tree_helper(inorder, i_f, i_t, preorder, p_f, p_t, inorder_index)
+  if i_f > i_t || p_f > p_t
+    return nil
+  end
+
+  if i_f == i_t && p_f == p_t
+    return TreeNode.new(inorder[i_f])
+  end
+
+  pivot = inorder_index[preorder[0]]
+  left_length = pivot - i_f
+  right_length = i_t - pivot
+
+  root = TreeNode.new(preorder[0])
+  root.left = build_tree_helper(inorder, i_f, pivot-1, preorder, 1, left_length, inorder_index)
+  root.rignt = build_tree_helper(inorder, pivot+1, i_t, preorder, left_length+1, p_t, inorder_index)
+
+  return root
+end
---
+++ test/construct_binary_tree_from_preorder_and_inorder_traversal_test.rb
@@ -0,0 +1,71 @@
+require 'minitest/autorun'
+require '../construct_binary_tree_from_preorder_and_inorder_traversal'
+
+describe 'build_tree(preorder, inorder)' do
+  it 'should work1' do
+    inorder = [4,2,6,5,7,1,3]
+    preorder = [1,2,4,5,6,7,3]
+    expected_result = [1,2,3,4,5,nil,nil,nil,nil,6,7]
+    #build_tree(preorder, inorder)
+    root = build_tree(preorder, inorder)
+    actual_result = convert_tree_to_array(root)
+    actual_result.must_equal(expected_result)
+  end
+end
+
+def convert_array_to_tree(array)
+  return nil if array.empty?
+  array = array.map do |val|
+    if !val.nil?
+      TreeNode.new(val)
+    else
+      nil
+    end
+  end
+
+  array.length.times do |i|
+    if !array[i].nil?
+      if !array[(i + 1) * 2 - 1].nil?
+        array[i].left = array[(i + 1) * 2 - 1]
+      end
+      if !array[(i + 1) * 2].nil?
+        array[i].right = array[(i + 1) * 2]
+      end
+    end
+  end
+
+  return array[0]
+end
+
+def convert_tree_to_array(root)
+  return [] if root.nil?
+  node_to_idx = {root => 1}
+  idx_to_val = {}
+  queue = [root]
+  max_idx = 1
+
+  while !queue.empty? do
+    n = queue.shift
+    idx = node_to_idx[n]
+    idx_to_val[idx] = n.val
+
+    max_idx = [max_idx, idx].max
+
+    if !n.left.nil?
+      queue << n.left
+      node_to_idx[n.left] = idx * 2
+    end
+
+    if !n.right.nil?
+      queue << n.right
+      node_to_idx[n.right] = idx * 2 + 1
+    end
+  end
+
+  array = max_idx.times.map {nil}
+  idx_to_val.each do |k, v|
+    array[k-1] = v
+  end
+
+  return array
+end
