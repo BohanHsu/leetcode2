95ab195d88ac93dbad2130806869464db90df230
U-NETAPP\bohan <bohan@VIDHIB-PC.hq.netapp.com>
2016-08-16 19:15:05 -0400

    wip

---
+++ text_justification.rb
@@ -0,0 +1,17 @@
+# @param {String[]} words
+# @param {Integer} max_width
+# @return {String[]}
+def full_justify(words, max_width)
+  i = 0
+  arr = []
+  while i < words.length do
+    cur = []
+    cur_len = 0
+    while i < words.length && cur_len < max_width do
+      if cur_len + words[i].length < max_width
+        cur_len += (1 + )
+      else
+      end
+    end
+  end
+end
