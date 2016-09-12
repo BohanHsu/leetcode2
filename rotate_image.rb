6869a64856db21982582dbac8f5473a4206f9f34
BohanHsu <ilovebarce@gmail.com>
2016-09-11 22:33:08 -0400

    ac

--- rotate_image.rb
+++ rotate_image.rb
@@ -1,31 +1,19 @@
 # @param {Integer[][]} matrix
 # @return {Void} Do not return anything, modify matrix in-place instead.
 def rotate(matrix)
-  matrix.each do |row|
-    puts "#{row}"
-  end
   (matrix.length/2).times do |i|
     (i...(matrix.length-1-i)).each do |j|
       rotate_helper(matrix, i, j)
     end
   end
+
 end
 
 def rotate_helper(matrix, i, j)
-  puts "+++++  i=#{i}, j=#{j}"
-  length = matrix.length
+  l = matrix.length
   tmp = matrix[i][j]
-  puts "tmp=#{matrix[i][j]}"
-  matrix[i][j] = matrix[length-1-i-j][j]
-  puts "matrix[#{i}][#{j}]=matrix[#{length-1-i-j}][#{j}] (#{matrix[length-1-i-j][j]})"
-  matrix[length-1-i-j][j] = matrix[length-1-i][length-1-i-j]
-  puts "matrix[#{length-1-i-j}][#{j}]=matrix[#{length-1-i}][#{length-1-i-j}] (#{matrix[length-1-i][length-1-i-j]})"
-  matrix[length-1-i][length-1-i-j] = matrix[i+j][length-1-i]
-  puts "matrix[#{length-1-i}][#{length-1-i-j}]=matrix[#{i+j}][#{length-1-i}] (#{matrix[i+j][length-1-i]})"
-  matrix[i+j][length-1-i] = tmp
-  puts "matrix[#{i+j}][#{length-1-i}]=#{tmp}"
+  matrix[i][j] = matrix[l-1-j][i]
+  matrix[l-1-j][i] = matrix[l-1-i][l-1-j]
+  matrix[l-1-i][l-1-j] = matrix[j][l-1-i]
+  matrix[j][l-1-i] = tmp
 end
-
-# length
-# i, j -> , i + j, length-1-i -> length-1-i, length-1-i-j -> length-1-i-j, j
-#0,0 -> 0,1 -> 1,1 -> 0,1
--- test/rotate_image_test.rb
+++ test/rotate_image_test.rb
@@ -31,7 +31,9 @@ describe 'rotate(matrix)' do
     ]
     rotate(matrix)
     matrix.must_equal(expected_result)
+  end
 
+  it 'should work2' do
     matrix = [
       [1,2,3,4],
       [5,6,7,8],
@@ -39,25 +41,10 @@ describe 'rotate(matrix)' do
       [13,14,15,16]
     ]
     expected_result = [
-      [13,5,9,1],
+      [13,9,5,1],
       [14,10,6,2],
       [15,11,7,3],
       [16,12,8,4],
-    ]
-    rotate(matrix)
-    matrix.must_equal(expected_result)
-  end
-
-  it 'should work2' do
-    matrix = [
-      [1,2,3],
-      [4,5,6],
-      [7,8,9]
-    ]
-    expected_result = [
-      [7,4,1],
-      [8,5,2],
-      [9,6,3]
     ]
     rotate(matrix)
     matrix.must_equal(expected_result)
